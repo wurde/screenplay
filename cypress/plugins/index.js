72d2b29d2c711c68dfb5f04086ff316adcf8ed2a
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-09-21 07:21:32 -0500

    setup env for cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,6 +12,12 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const path = require("path");
+
+require("dotenv").config({
+  path: path.join(__dirname, "..", "..", ".env.local"),
+});
+
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -19,4 +25,9 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+
+  config.env.preview_email = process.env.PREVIEW_EMAIL;
+  config.env.preview_password = process.env.PREVIEW_PASSWORD;
+
+  return config;
+};
